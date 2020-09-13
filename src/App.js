79d0dce2b23c0cdaefeb520f9581a2b96d18d7d2
BHARATH SIMHA REDDY <bharathreddy8345@gmail.com>
2020-09-13 13:10:46 +0530

    prepared all the basic outline of the app and ready for firebase connectivity

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Post from "./Post";
+
+import "./App.css";
 
 function App() {
+  const [post, setpost] = useState([
+    {
+      username: "Simha",
+      caption: "Demon Slayer",
+      imageurl:
+        "https://i1.wp.com/recommendmeanime.com/wp-content/uploads/2019/04/demon-slayer-anime.jpeg?fit=1280%2C718&ssl=1",
+    },
+    {
+      username: "Bharath",
+      caption: "I loveit!",
+      imageurl:
+        "https://cosmosmagazine.com/wp-content/uploads/2019/12/GettyImages-691120979-1440x1079.jpg",
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <img
+          className="app__header-image"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        />
+      </div>
+
+      {post.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageurl={post.imageurl}
+        />
+      ))}
+
+      {/* <Post
+        username="Reddy"
+        caption="It's accelerating"
+        imageurl="https://images7.alphacoders.com/102/thumb-1920-1027364.png"
+      />
+      <Post
+        username="bharath.simha.reddy"
+        caption="YAY! spider"
+        imageurl="https://i.pinimg.com/originals/bf/82/f6/bf82f6956a32819af48c2572243e8286.jpg"
+      /> */}
     </div>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import "./Post.css";
+
+const Post = ({ username, imageurl, caption }) => {
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt={username}
+          src="/static/images/avatar/1.jpg"
+        />
+        <h3>{username}</h3>
+      </div>
+
+      <img className="post__image" src={imageurl} alt="" />
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>
+    </div>
+  );
+};
+
+export default Post;
